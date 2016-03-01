21e81d1de6ae67094ca51b44d60b68e365726a8c
nik <nik@parkellipsen.de>
2016-03-01 23:00:48 +0100

    explicit synthdef path

--- start_thenemin.sh
+++ start_thenemin.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export THENEMIN_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" && 
+export SC_SYNTHDEF_PATH=$THENEMIN_ROOT/synthdefs
 jackd -R -P99 -dalsa -dhw:0 -r44100 -p512 -n4 &
 sleep 4
 scsynth -u 57110 &
